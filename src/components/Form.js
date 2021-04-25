b1c67ad0d42c35f5b37b2136af4c43d4b08a9247
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-04-25 12:23:40 +0200

    added input

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+export const Form = () => {
+
+    return (
+        <form>
+            <div className="form-group">
+                <input
+                 type="text"
+                  className="form-control"
+                  placeholder="Write Name of search"
+                  />
+            </div>
+        </form>
+    )
+}
