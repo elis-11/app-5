15cfb3448d14d4a2bc26680bcb64e91be5c0c0e8
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-04-26 20:24:57 +0200

    added Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,16 +1,26 @@
-export const Alert = ({alert}) => {
+import React, { useContext } from "react";
+import { AlertContext } from "../context/alert/alertContext";
 
-  if (!alert) {
-    return null
+export const Alert = () => {
+  const { alert, hide } = useContext(AlertContext);
+
+  if (!alert.visible) {
+    return null;
   }
 
-    return (
-        <div className={`alert alert-${alert.type || 'warning'} alert-dismissible`}>
-  <strong>Warning!</strong> 
+  return (
+    <div className={`alert alert-${alert.type || "warning"} alert-dismissible`}>
+      <strong>Warning!</strong>
       {alert.text}
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-</div>
-    )
-}
+      <button
+        onClick={hide}
+        type="button"
+        className="close"
+        data-dismiss="alert"
+        aria-label="Close"
+      >
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  );
+};
