3196df64f31b5b3dbe8160b87d7f11a28f02f01c
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-04-26 20:27:41 +0200

    modified logic with Alert!!!!!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 // eslint-disable-next-line no-unused-vars
-import logo from './logo.svg';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
-import { Home } from './pages/Home';
-import { About } from './pages/About';
-import { Navbar } from './components/Navbar';
-import { Alert } from './components/Alert';
+import logo from "./logo.svg";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Navbar } from "./components/Navbar";
+import { Alert } from "./components/Alert";
+import { AlertState } from "./context/alert/AlertState";
 
 function App() {
   return (
-    <BrowserRouter>
-    <Navbar />
-      <div className="container pt-4">
-        <Alert />
-        <Switch>
-          <Route path={'/'} exact component={Home} />
-          <Route path={'/about'} component={About} />
-        </Switch>
-      </div> 
-    </BrowserRouter>
+    <AlertState>
+      <BrowserRouter>
+        <Navbar />
+        <div className="container pt-4">
+          <Alert />
+          <Switch>
+            <Route path={"/"} exact component={Home} />
+            <Route path={"/about"} component={About} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </AlertState>
   );
 }
 
